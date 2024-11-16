a0829793ed2f519c45f1a939d89dfce77a1145f0
supernova2001 <janakiramsharma18@gmail.com>
2024-11-16 14:17:26 -0500

    added /api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 require("dotenv").config();
 const { Sequelize, DataTypes } = require("sequelize");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 app.use(express.json());
+app.use('/api',userRoute);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
